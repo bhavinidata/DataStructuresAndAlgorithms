999436dff882bdc8a236ca9065865ece692a0ec3
bhavini <bhavini266@gmail.com>
2020-04-04 10:59:57 -0500

    added faster solution using hash_table

--- Arrays/TwoSum.py
+++ Arrays/TwoSum.py
@@ -1,29 +1,37 @@
 from typing import List
+
 class Solution:
-    # def twoSum(self, nums: List[int], target: int) -> List[int]:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        resultSum = []
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                result = nums[i] +nums[j]
-                if result == target:
-                    resultSum.append(i)
-                    resultSum.append(j)
-                    return resultSum
-
-if __name__ == '__main__':
-    re =  Solution().twoSum([2, 7, 11, 15], 9)
-    print(re)
-    re1 =  Solution().twoSum([0, -4, 11, 4], 0)
-    print(re1)
-
+        # # Brute force method
         # resultSum = []
-        # while i < len(nums):
-        #     while j < len(nums):
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
         #         result = nums[i] +nums[j]
         #         if result == target:
-        #             resultSum.append(nums[i])
-        #             resultSum.append(nums[j])
+        #             resultSum.append(i)
+        #             resultSum.append(j)
+        #             return resultSum
+
+        # get empty dictionary/hashmap
+        hash_table = {}
+        # Loop through the input list of numbers and index
+        for i, num in enumerate(nums):
+            # subtract number from target number
+            # check if the number is present in has table key
+            if (target-num) in hash_table:
+                # if it is there then return the value of that key (which is stored as hash_table       value)
+                return(hash_table[target-num], i)
+                break
+            hash_table[num] = i
+        return([])
+
+if __name__ == '__main__':
+    result =  Solution().twoSum([2, 7, 11, 15], 9)
+    print(result)
+    result1 =  Solution().twoSum([0, -4, 11, 4], 0)
+    print(result1)
+    result2 =  Solution().twoSum([5, -4, 11, 4, 10], 6)
+    print(result2)
         
 
 
